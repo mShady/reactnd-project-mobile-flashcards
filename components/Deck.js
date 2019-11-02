1c03e2781cca2ec94e1c53f394b4c62f1dd70f38
Shady M. Najib <shady@comcal.org>
2019-11-02 21:49:40 +0200

    Add Decks List Item

---
+++ components/Deck.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+
+class Deck extends Component {
+  render() {
+    const { deck } = this.props;
+    console.log("deck", deck);
+    return (
+      <TouchableOpacity
+        onPress={() =>
+          this.props.navigation.navigate("EntryDetail", { deckId: deck.title })
+        }
+      >
+        <Text style={styles.text}>
+          {deck.title} - {deck.questions.length} cards
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+function mapStateToProps(decks, { deckId }) {
+  return {
+    deck: decks[deckId]
+  };
+}
+
+export default connect(mapStateToProps)(Deck);
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    color: "black",
+    lineHeight: 24,
+    textAlign: "center",
+    margin: 20
+  }
+});
