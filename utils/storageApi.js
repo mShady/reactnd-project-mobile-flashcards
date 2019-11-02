77f48ab0bc85e7c3f0882294312beb0315d66bdf
Shady M. Najib <shady@comcal.org>
2019-11-02 19:26:31 +0200

    Update storage API

--- utils/storageApi.js
+++ utils/storageApi.js
@@ -1,20 +1,43 @@
 import { AsyncStorage } from "react-native";
 const MOBILE_FLASHCARDS_STORAGE_KEY = "MOBILE_FLASHCARDS_STORAGE_KEY";
 
-export function submitEntry({ key, entry }) {
+export function getDecks() {
+  return AsyncStorage.getItem(MOBILE_FLASHCARDS_STORAGE_KEY).then(results => {
+    const data = JSON.parse(results);
+    return data;
+  });
+}
+
+export function getDeck(key) {
+  return AsyncStorage.getItem(MOBILE_FLASHCARDS_STORAGE_KEY).then(results => {
+    const data = JSON.parse(results);
+    return data[key];
+  });
+}
+
+export function saveDeckTitle(title) {
   return AsyncStorage.mergeItem(
     MOBILE_FLASHCARDS_STORAGE_KEY,
-    JSON.stringify({
-      [key]: entry
-    })
+    JSON.stringify(deckFactory(title))
   );
 }
 
-export function removeEntry(key) {
+export function addCardToDeck(title, card) {
   return AsyncStorage.getItem(MOBILE_FLASHCARDS_STORAGE_KEY).then(results => {
     const data = JSON.parse(results);
-    data[key] = undefined;
-    delete data[key];
+    data[title] = {
+      ...data[title],
+      questions: data[title].questions.concat(card)
+    };
     AsyncStorage.setItem(MOBILE_FLASHCARDS_STORAGE_KEY, JSON.stringify(data));
   });
 }
+
+const deckFactory = title => ({
+  [title]: {
+    React: {
+      title,
+      questions: []
+    }
+  }
+});
